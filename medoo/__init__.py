a8292d5c6491529f985268e645a00b992c494591
pwwang <pwwang@pwwang.com>
2018-04-30 15:28:54 -0500

    Add setup.py

--- medoo/__init__.py
+++ medoo/__init__.py
@@ -1,4 +1,6 @@
+VERSION = '0.0.0alpha'
 # expose Field, so it can be used as value in update set
+
 from .medooBuilder import Builder, Function, Raw, Field, Table
 from .medooSqlite import MedooSqlite
 from .medooBase import Box
@@ -8,15 +10,15 @@ DATABASE_TYPES = {
 }
 
 class Medoo(object):
-	
+
 	def __new__(klass, *args, **kwargs):
 		if 'database_type' not in kwargs:
 			raise ValueError('No database type specified.')
-		
+
 		dbtype = kwargs['database_type']
 		del kwargs['database_type']
 		if dbtype not in [dbtype for dblist in DATABASE_TYPES.values() for dbtype in dblist]:
 			raise ValueError('Database type not supported: %s.' % dbtype)
-			
+
 		if dbtype in DATABASE_TYPES['MedooSqlite']:
 			return MedooSqlite(*args, **kwargs)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# get version
+from os import path
+verfile = path.join(path.dirname(__file__), 'medoo', '__init__.py')
+with open(verfile) as vf:
+    VERSION = vf.readline().split('=')[1].strip()[1:-1]
+
+setup (
+	name             = 'pymedoo',
+	version          = VERSION,
+	description      = "A lightweight database framework for python",
+	url              = "https://github.com/pwwang/pymedoo",
+	author           = "pwwang",
+	author_email     = "pwwang@pwwang.com",
+	license          = "MIT License",
+	packages         = find_packages(),
+	install_requires = [
+		'six'
+    ],
+)
