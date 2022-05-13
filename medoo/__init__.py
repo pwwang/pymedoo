0c0a43ba45e2ef95ab953722e40ca28f2757e1f5
pwwang <pwwang@pwwang.com>
2022-05-12 21:47:09 -0700

    🔖 0.1.0

--- medoo/__init__.py
+++ medoo/__init__.py
@@ -4,7 +4,7 @@ import importlib
 from .builder import Raw, Table, Field
 from .dialect import Dialect
 
-__version__ = "0.0.8"
+__version__ = "0.1.0"
 
 DATABASE_TYPES = {
     "Sqlite": ["sqlite", "sqlite3"],
